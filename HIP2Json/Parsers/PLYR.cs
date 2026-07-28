fbd2514252abae5b51b093b348e57f3dca07cd60
AidenFliss <92261372+AidenFliss@users.noreply.github.com>
2026-07-28 19:39:39 -0400

    Fix missing PLYR asset id

--- HIP2Json/Parsers/PLYR.cs
+++ HIP2Json/Parsers/PLYR.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text.Json.Serialization;
 
 namespace HIP2Json;
 
@@ -6,7 +7,7 @@ public sealed class PLYRParser : AssetParser
 {
     public override object Parse(BinaryReader br, long assetStart, long dataStart)
     {
-        return new PLYR { };
+        return new PLYR { lightKitID = ReadUInt32BE(br) };
     }
 
     public override object Serialize(object obj)
@@ -16,8 +17,14 @@ public sealed class PLYRParser : AssetParser
         using var ms = new MemoryStream();
         using var bw = new BinaryWriter(ms);
 
+        WriteUInt32BE(bw, plyr.lightKitID);
+
         return ms.ToArray();
     }
 }
 
-public class PLYR { }
+public class PLYR
+{
+    [JsonConverter(typeof(AssetIDConverter))]
+    public uint lightKitID { get; set; }
+}
