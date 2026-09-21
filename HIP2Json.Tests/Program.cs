ddbe7be58b420502b006c5ca09f6e6198a51e9f8
AidenFliss <92261372+AidenFliss@users.noreply.github.com>
2026-09-21 06:08:29 -0400

    Fix HIP2Json.Tests/Program.cs build: add missing using System.Diagnostics for Stopwatch calls

--- HIP2Json.Tests/Program.cs
+++ HIP2Json.Tests/Program.cs
@@ -1,12 +1,8 @@
 ﻿#nullable enable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Encodings.Web;
-using HIP2Json;
 
 namespace HIP2Json.Tests;
 
